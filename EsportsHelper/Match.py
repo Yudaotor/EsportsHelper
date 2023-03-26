88c24cdbd513f60d555516abc8ee19d829009540
Yudaotor <yudaotor@qq.com>
2023-03-26 14:44:43 +0800

    增加检查掉落功能，优化提示

--- EsportsHelper/Match.py
+++ EsportsHelper/Match.py
@@ -38,7 +38,7 @@ class Match:
                         self.OVERRIDES[temp[0]] = temp[1]
         except Exception as ex:
             traceback.print_exc()
-            print(f"[red]获取文件失败,请检查网络[/red]")
+            print(f"[red]获取文件失败,请检查网络是否能连上github[/red]")
             input("按任意键退出")
 
     def watchMatches(self, delay):
@@ -46,8 +46,18 @@ class Match:
         self.mainWindow = self.driver.current_window_handle
         while True:
             try:
+                self.log.info("开始检查直播...")
+                print(f"[green]开始检查直播...[/green]")
+
                 self.driver.switch_to.window(self.mainWindow)
+                isDrop, imgUrl, title = self.rewards.checkNewDrops()
+                if isDrop:
+                    self.log.info(f"发现新的掉落: {title}")
+                    print(f"[blue]发现新的掉落: {title}[/blue]")
+                    if self.config.connectorDropsUrl != "":
+                        self.rewards.notifyDrops(imgUrl=imgUrl, title=title)
                 time.sleep(3)
+
                 self.driver.get("https://lolesports.com/schedule")
                 time.sleep(5)
                 liveMatches = self.getMatches()
@@ -58,9 +68,12 @@ class Match:
                 else:
                     self.log.info(f"现在有 {len(liveMatches)} 个赛区正在直播中")
                     print(f"[green]现在有 {len(liveMatches)} 个赛区正在直播中[/green]")
+
                 self.closeTabs(liveMatches=liveMatches)
+
                 self.openNewMatches(liveMatches=liveMatches, disWatchMatches=self.config.disWatchMatches)
                 time.sleep(3)
+
                 self.driver.switch_to.window(self.mainWindow)
                 self.log.info(f"下一次检查在: {datetime.now() + timedelta(seconds=delay)}")
                 self.log.debug("============================================")
@@ -99,7 +112,7 @@ class Match:
                     else:
                         match = splitUrl[-1]
                     self.log.info(f"{match} 比赛结束")
-                    print(f"[yellow]{match} 比赛结束[/green]")
+                    print(f"[yellow]{match} 比赛结束[/yellow]")
                     self.driver.close()
                     removeList.append(k)
                     self.driver.switch_to.window(self.mainWindow)
@@ -116,6 +129,7 @@ class Match:
     def openNewMatches(self, liveMatches, disWatchMatches):
         newLiveMatches = set(liveMatches) - set(self.currentWindows.keys())
         for match in newLiveMatches:
+
             flag = True
             for disMatch in disWatchMatches:
                 if match.find(disMatch) != -1:
@@ -130,34 +144,40 @@ class Match:
                     break
             if not flag:
                 continue
+
             self.driver.switch_to.new_window('tab')
-            time.sleep(2)
+            time.sleep(1)
             self.currentWindows[match] = self.driver.current_window_handle
             if match in self.OVERRIDES:
                 url = self.OVERRIDES[match]
                 self.driver.get(url)
-
                 self.rewards.checkRewards(url)
                 try:
-                    self.twitch.setTwitchQuality()
-                    self.log.info("Twitch 清晰度设置成功")
-                    print("[green]Twitch 清晰度设置成功")
-                except TimeoutException:
-                    self.log.critical("无法设置 Twitch 清晰度. 这场比赛是在 Twitch 上吗?")
-                    print("[red]无法设置 Twitch 清晰度. 这场比赛是在 Twitch 上吗?")
+                    if self.twitch.setTwitchQuality():
+                        self.log.info("Twitch 清晰度设置成功")
+                        print("[green]Twitch 清晰度设置成功")
+                    else:
+                        self.log.critical("Twitch 清晰度设置失败")
+                        print("[red]Twitch 清晰度设置失败")
+                except Exception:
+                    self.log.critical("无法设置 Twitch 清晰度.")
+                    print("[red]无法设置 Twitch 清晰度.")
                     traceback.print_exc()
                     self.log.error(traceback.format_exc())
             else:
                 url = match
                 self.driver.get(url)
                 try:
-                    self.youtube.setYoutubeQuality()
+                    if self.youtube.setYoutubeQuality():
+                        self.log.info("Youtube 清晰度设置成功")
+                        print("[green]Youtube 清晰度设置成功")
+                    else:
+                        self.log.critical("Youtube 清晰度设置失败")
+                        print("[red]Youtube 清晰度设置失败")
                     self.rewards.checkRewards(url)
-                    self.log.info("Youtube 清晰度设置成功")
-                    print("[green]Youtube 清晰度设置成功")
-                except TimeoutException:
-                    self.log.critical(f"无法设置 Youtube 清晰度. 这场比赛是在 Youtube 上吗?")
-                    print("[red]无法设置 Youtube 清晰度. 这场比赛是在 Youtube 上吗?")
+                except Exception:
+                    self.log.critical(f"无法设置 Youtube 清晰度.")
+                    print("[red]无法设置 Youtube 清晰度.")
                     traceback.print_exc()
                     self.log.error(traceback.format_exc())
             time.sleep(5)
