7bb535b8f6a34e0fb36c35c8f6f33276ccbe3bf9
Yudaotor <yudaotor@qq.com>
2023-03-27 10:44:51 +0800

    移除2FA

--- EsportsHelper/LoginHandler.py
+++ EsportsHelper/LoginHandler.py
@@ -31,29 +31,11 @@ class LoginHandler:
             submitButton = wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "button[type=submit]")))
             time.sleep(1)
             self.driver.execute_script("arguments[0].click();", submitButton)
-            self.log.debug("(∩_∩) 账密 提交成功")
-            print("[green](∩_∩) 账密 提交成功")
+            self.log.debug("∩_∩ 账密 提交成功")
+            print("[green]∩_∩ 账密 提交成功")
             time.sleep(5)
-            if len(self.driver.find_elements(by=By.CSS_SELECTOR, value="div.text__web-code")) > 0:
-                self.insertCode()
             wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "div.riotbar-summoner-name")))
         except Exception as e:
             traceback.print_exc()
             self.log.error(traceback.format_exc())
 
-    def insertCode(self):
-        try:
-            wait = WebDriverWait(self.driver, 20)
-            authText = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "h5.grid-panel__subtitle")))
-            self.log.info(f'请输入二级验证代码 ({authText.text})')
-            print(f'[yellow]请输入二级验证代码 ({authText.text}')
-            code = input('二级验证代码: ')
-            codeInput = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "div.codefield__code--empty > div > input")))
-            codeInput.send_keys(code)
-            submitButton = wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, "button[type=submit]")))
-            self.driver.execute_script("arguments[0].click();", submitButton)
-            self.log.debug("二级验证代码 提交成功")
-            print("[green]二级验证代码 提交成功")
-        except Exception as e:
-            traceback.print_exc()
-            self.log.error(traceback.format_exc())
