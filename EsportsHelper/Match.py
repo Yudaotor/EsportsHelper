b85bd2827285a22e3f7434db5143b788da2d4f02
Yudaotor <yudaotor@qq.com>
2023-04-02 13:37:18 +0800

    feat: show next match time
    添加下一场比赛时间的显示,同时增加了一些错误提示

--- EsportsHelper/Match.py
+++ EsportsHelper/Match.py
@@ -3,17 +3,15 @@ from datetime import datetime, timedelta
 from random import randint
 from time import sleep
 from traceback import format_exc, print_exc
-
 import requests
 from rich import print
-from selenium.common import WebDriverException
+from selenium.common import WebDriverException, NoSuchWindowException
 from selenium.webdriver.common.by import By
 from urllib3.exceptions import MaxRetryError
 
 from EsportsHelper.Rewards import Rewards
 from EsportsHelper.Twitch import Twitch
-from EsportsHelper.Utils import Quit
-from EsportsHelper.Utils import desktopNotify
+from EsportsHelper.Utils import sysQuit, desktopNotify, downloadOverrideFile, Utils
 from EsportsHelper.Youtube import Youtube
 
 
@@ -27,33 +25,9 @@ class Match:
         self.youtube = Youtube(driver=driver, log=log)
         self.currentWindows = {}
         self.mainWindow = self.driver.current_window_handle
-        self.OVERRIDES = {}
+        self.OVERRIDES = downloadOverrideFile()
         self.retryTimes = 3
-        try:
-            req = requests.session()
-            headers = {'Content-Type': 'text/plain; charset=utf-8',
-                       'Connection': 'close'}
-            remoteOverrideFile = req.get(
-                "https://raw.githubusercontent.com/Yudaotor/EsportsHelper/main/override.txt", headers=headers)
-            if remoteOverrideFile.status_code == 200:
-                override = remoteOverrideFile.text.split(",")
-                first = True
-                for o in override:
-                    temp = o.split("|")
-                    if len(temp) == 2:
-                        if first:
-                            first = False
-                        else:
-                            temp[0] = temp[0][1:]
-                        self.OVERRIDES[temp[0]] = temp[1]
-        except MaxRetryError:
-            self.log.error("获取文件失败, 请稍等再试")
-            print(f"[red]〒.〒 获取文件失败, 请稍等再试[/red]")
-            input("按任意键退出")
-        except Exception as ex:
-            print_exc()
-            print(f"[red]〒.〒 获取文件失败,请检查网络是否能连上github[/red]")
-            input("按任意键退出")
+        self.utils = Utils(config=config)
 
     def watchMatches(self, delay, maxRunHours):
         self.currentWindows = {}
@@ -66,7 +40,7 @@ class Match:
                 self.log.info("●_● 开始检查直播...")
                 print(f"[green]●_● 开始检查直播...[/green]")
                 self.driver.switch_to.window(self.mainWindow)
-                isDrop, poweredByImg, productImg, unlockedDate, eventTitle, dropItem, dropItemImg = self.rewards.checkNewDrops()
+                isDrop, poweredByImg, productImg, rare, eventTitle, unlockedDate, dropItem, dropItemImg = self.rewards.checkNewDrops()
                 if isDrop:
                     for i in range(len(poweredByImg)):
                         self.log.info(f"ΩДΩ [{self.config.username}]通过事件{eventTitle[i]} 获得{dropItem[i]} {unlockedDate[i]}")
@@ -74,13 +48,13 @@ class Match:
                         if self.config.desktopNotify:
                             desktopNotify(poweredByImg[i], productImg[i], unlockedDate[i], eventTitle[i], dropItem[i], dropItemImg[i])
                         if self.config.connectorDropsUrl != "":
-                            self.rewards.notifyDrops(poweredByImg[i], productImg[i], unlockedDate[i], eventTitle[i], dropItem[i], dropItemImg[i])
+                            self.rewards.notifyDrops(poweredByImg[i], productImg[i], rare[i], eventTitle[i], unlockedDate[i], dropItem[i], dropItemImg[i])
                 sleep(3)
                 try:
                     self.driver.get("https://lolesports.com/schedule?leagues=lcs,north_american_challenger_league,lcs_challengers_qualifiers,college_championship,cblol-brazil,lck,lcl,lco,lec,ljl-japan,lla,lpl,pcs,turkiye-sampiyonluk-ligi,vcs,worlds,all-star,european-masters,lfl,nlc,elite_series,liga_portuguesa,pg_nationals,ultraliga,superliga,primeleague,hitpoint_masters,esports_balkan_league,greek_legends,arabian_league,lck_academy,ljl_academy,lck_challengers_league,cblol_academy,liga_master_flo,movistar_fiber_golden_league,elements_league,claro_gaming_stars_league,honor_division,volcano_discover_league,honor_league,msi,tft_esports")
                 except Exception as e:
                     self.driver.get("https://lolesports.com/schedule")
-                sleep(5)
+                sleep(4)
                 liveMatches = self.getMatchInfo()
                 sleep(3)
                 if len(liveMatches) == 0:
@@ -99,6 +73,7 @@ class Match:
                 randomDelay = randint(int(delay * 0.08), int(delay * 0.15))
                 newDelay = randomDelay * 10
                 self.driver.switch_to.window(self.mainWindow)
+                self.checkNextMatch()
                 self.log.info(
                     f"下一次检查在: {datetime.now() + timedelta(seconds=newDelay)}")
                 self.log.debug("============================================")
@@ -107,17 +82,17 @@ class Match:
                 print(f"[green]============================================[/green]")
                 sleep(newDelay)
                 self.retryTimes = 3
-            except WebDriverException as e:
+            except NoSuchWindowException as e:
                 self.retryTimes -= 1
-                self.log.error("Q_Q webdriver发生错误, 重试中")
-                print(f"[red]Q_Q webdriver发生错误, 重试中[/red]")
+                self.log.error("Q_Q 找不到对应窗口, 重试中")
+                print(f"[red]Q_Q 找不到对应窗口, 重试中[/red]")
                 print_exc()
                 sleep(2)
                 if self.retryTimes <= 0:
-                    self.log.error("Q_Q webdriver发生错误, 将于3秒后退出...")
-                    print(f"[red]Q_Q webdriver发生错误, 将于3秒后退出...[/red]")
-                    Quit(self.driver, format_exc())
-
+                    self.utils.errorNotify(e="Q_Q 找不到对应窗口")
+                    self.log.error("Q_Q 找不到对应窗口, 将于3秒后退出...")
+                    print(f"[red]Q_Q 找不到对应窗口, 将于3秒后退出...[/red]")
+                    sysQuit(self.driver, format_exc())
             except Exception as e:
                 self.retryTimes -= 1
                 self.log.error("Q_Q 发生错误")
@@ -125,9 +100,10 @@ class Match:
                 print_exc()
                 sleep(2)
                 if self.retryTimes <= 0:
+                    self.utils.errorNotify(e="Q_Q 发生错误")
                     self.log.error("Q_Q 发生错误, 将于3秒后退出...")
                     print(f"[red]Q_Q 发生错误, 将于3秒后退出...[/red]")
-                    Quit(self.driver, format_exc())
+                    sysQuit(self.driver, format_exc())
 
     def getMatchInfo(self):
         try:
@@ -149,6 +125,7 @@ class Match:
             removeList = []
             for k in self.currentWindows.keys():
                 self.driver.switch_to.window(self.currentWindows[k])
+                sleep(1)
                 if k not in liveMatches:
                     splitUrl = k.split('/')
                     if splitUrl[-2] != "live":
@@ -159,8 +136,9 @@ class Match:
                     print(f"[yellow]̋0.0 {match} 比赛结束[/yellow]")
                     self.driver.close()
                     removeList.append(k)
+                    sleep(2)
                     self.driver.switch_to.window(self.mainWindow)
-                    sleep(5)
+                    sleep(3)
                 else:
                     self.rewards.checkRewards(k)
             for k in removeList:
@@ -168,12 +146,14 @@ class Match:
             self.driver.switch_to.window(self.mainWindow)
         except Exception as e:
             print_exc()
+            print(f"[red]Q_Q 关闭已结束的比赛时发送错误[/red]")
+            self.utils.errorNotify(e="Q_Q 关闭已结束的比赛时发送错误")
             self.log.error(format_exc())
 
     def startWatchNewMatches(self, liveMatches, disWatchMatches):
         newLiveMatches = set(liveMatches) - set(self.currentWindows.keys())
         for match in newLiveMatches:
-
+            self.log.info(match)
             flag = True
             for disMatch in disWatchMatches:
                 if match.find(disMatch) != -1:
@@ -202,10 +182,10 @@ class Match:
                         self.log.info(">_< Twitch 160p清晰度设置成功")
                         print("[green]>_< Twitch 160p清晰度设置成功")
                     else:
-                        self.log.critical("°D° Twitch 清晰度设置失败")
+                        self.log.error("°D° Twitch 清晰度设置失败")
                         print("[red]°D° Twitch 清晰度设置失败")
                 except Exception:
-                    self.log.critical("°D° 无法设置 Twitch 清晰度.")
+                    self.log.error("°D° 无法设置 Twitch 清晰度.")
                     print("[red]°D° 无法设置 Twitch 清晰度.")
                     print_exc()
                     self.log.error(format_exc())
@@ -217,12 +197,28 @@ class Match:
                         self.log.info(">_< Youtube 144p清晰度设置成功")
                         print("[green]>_< Youtube 144p清晰度设置成功")
                     else:
-                        self.log.critical("°D° Youtube 清晰度设置失败")
+                        self.log.error("°D° Youtube 清晰度设置失败")
                         print("[red]°D° Youtube 清晰度设置失败")
                     self.rewards.checkRewards(url)
                 except Exception:
-                    self.log.critical(f"°D° 无法设置 Youtube 清晰度.")
+                    self.log.error(f"°D° 无法设置 Youtube 清晰度.")
                     print("[red]°D° 无法设置 Youtube 清晰度.")
                     print_exc()
                     self.log.error(format_exc())
             sleep(5)
+
+    def checkNextMatch(self):
+        try:
+            nextMatchDayTime = self.driver.find_element(
+                by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate > div.date > span.monthday").text
+            nextMatchTime = self.driver.find_element(
+                by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate + div.EventMatch > div > div.EventTime > div > span.hour").text
+            nextMatchAMOrPM = self.driver.find_element(
+                by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate + div.EventMatch > div > div.EventTime > div > span.hour ~ span.ampm").text
+            nextMatchLeague = self.driver.find_element(
+                by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate + div.EventMatch > div > div.league > div.name").text
+            print(f"[green]下一场比赛时间: 日期{nextMatchDayTime} 时间{nextMatchAMOrPM} {nextMatchTime}时 赛区{nextMatchLeague}[/green]")
+        except Exception:
+            self.log.error("Q_Q 获取下一场比赛时间失败")
+            print(f"[red]Q_Q 获取下一场比赛时间失败[/red]")
+            print_exc()
