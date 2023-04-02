8f883f8a78bf14774fd7afd25937995823d99306
Yudaotor <yudaotor@qq.com>
2023-04-02 20:34:17 +0800

    feat: config not show stream
    当配置为关闭视频流时去除对应元素,增加查询ampm的异常处理

--- EsportsHelper/Match.py
+++ EsportsHelper/Match.py
@@ -5,7 +5,7 @@ from time import sleep
 from traceback import format_exc, print_exc
 import requests
 from rich import print
-from selenium.common import WebDriverException, NoSuchWindowException
+from selenium.common import WebDriverException, NoSuchWindowException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from urllib3.exceptions import MaxRetryError
 
@@ -40,7 +40,7 @@ class Match:
                 self.log.info("●_● 开始检查直播...")
                 print(f"[green]●_● 开始检查直播...[/green]")
                 self.driver.switch_to.window(self.mainWindow)
-                isDrop, poweredByImg, productImg, rare, eventTitle, unlockedDate, dropItem, dropItemImg = self.rewards.checkNewDrops()
+                isDrop, poweredByImg, productImg, eventTitle, unlockedDate, dropItem, dropItemImg = self.rewards.checkNewDrops()
                 if isDrop:
                     for i in range(len(poweredByImg)):
                         self.log.info(f"ΩДΩ [{self.config.username}]通过事件{eventTitle[i]} 获得{dropItem[i]} {unlockedDate[i]}")
@@ -48,7 +48,7 @@ class Match:
                         if self.config.desktopNotify:
                             desktopNotify(poweredByImg[i], productImg[i], unlockedDate[i], eventTitle[i], dropItem[i], dropItemImg[i])
                         if self.config.connectorDropsUrl != "":
-                            self.rewards.notifyDrops(poweredByImg[i], productImg[i], rare[i], eventTitle[i], unlockedDate[i], dropItem[i], dropItemImg[i])
+                            self.rewards.notifyDrops(poweredByImg[i], productImg[i], eventTitle[i], unlockedDate[i], dropItem[i], dropItemImg[i])
                 sleep(3)
                 try:
                     self.driver.get("https://lolesports.com/schedule?leagues=lcs,north_american_challenger_league,lcs_challengers_qualifiers,college_championship,cblol-brazil,lck,lcl,lco,lec,ljl-japan,lla,lpl,pcs,turkiye-sampiyonluk-ligi,vcs,worlds,all-star,european-masters,lfl,nlc,elite_series,liga_portuguesa,pg_nationals,ultraliga,superliga,primeleague,hitpoint_masters,esports_balkan_league,greek_legends,arabian_league,lck_academy,ljl_academy,lck_challengers_league,cblol_academy,liga_master_flo,movistar_fiber_golden_league,elements_league,claro_gaming_stars_league,honor_division,volcano_discover_league,honor_league,msi,tft_esports")
@@ -178,12 +178,15 @@ class Match:
                 if not self.rewards.checkRewards(url):
                     return
                 try:
-                    if self.twitch.setTwitchQuality():
-                        self.log.info(">_< Twitch 160p清晰度设置成功")
-                        print("[green]>_< Twitch 160p清晰度设置成功")
+                    if self.config.closeStream:
+                        self.driver.execute_script("""var data=document.querySelector('#video-player').remove()""")
                     else:
-                        self.log.error("°D° Twitch 清晰度设置失败")
-                        print("[red]°D° Twitch 清晰度设置失败")
+                        if self.twitch.setTwitchQuality():
+                            self.log.info(">_< Twitch 160p清晰度设置成功")
+                            print("[green]>_< Twitch 160p清晰度设置成功")
+                        else:
+                            self.log.error("°D° Twitch 清晰度设置失败")
+                            print("[red]°D° Twitch 清晰度设置失败")
                 except Exception:
                     self.log.error("°D° 无法设置 Twitch 清晰度.")
                     print("[red]°D° 无法设置 Twitch 清晰度.")
@@ -193,13 +196,16 @@ class Match:
                 url = match
                 self.driver.get(url)
                 try:
-                    if self.youtube.setYoutubeQuality():
-                        self.log.info(">_< Youtube 144p清晰度设置成功")
-                        print("[green]>_< Youtube 144p清晰度设置成功")
+                    if self.config.closeStream:
+                        self.driver.execute_script("""var data=document.querySelector('#video-player').remove()""")
                     else:
-                        self.log.error("°D° Youtube 清晰度设置失败")
-                        print("[red]°D° Youtube 清晰度设置失败")
-                    self.rewards.checkRewards(url)
+                        if self.youtube.setYoutubeQuality():
+                            self.log.info(">_< Youtube 144p清晰度设置成功")
+                            print("[green]>_< Youtube 144p清晰度设置成功")
+                        else:
+                            self.log.error("°D° Youtube 清晰度设置失败")
+                            print("[red]°D° Youtube 清晰度设置失败")
+                        self.rewards.checkRewards(url)
                 except Exception:
                     self.log.error(f"°D° 无法设置 Youtube 清晰度.")
                     print("[red]°D° 无法设置 Youtube 清晰度.")
@@ -213,12 +219,14 @@ class Match:
                 by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate > div.date > span.monthday").text
             nextMatchTime = self.driver.find_element(
                 by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate + div.EventMatch > div > div.EventTime > div > span.hour").text
-            nextMatchAMOrPM = self.driver.find_element(
-                by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate + div.EventMatch > div > div.EventTime > div > span.hour ~ span.ampm").text
+            try:
+                nextMatchAMOrPM = self.driver.find_element(
+                    by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate + div.EventMatch > div > div.EventTime > div > span.hour ~ span.ampm").text
+            except NoSuchElementException:
+                nextMatchAMOrPM = ""
             nextMatchLeague = self.driver.find_element(
                 by=By.CSS_SELECTOR, value="div.divider.future + div.EventDate + div.EventMatch > div > div.league > div.name").text
             print(f"[green]下一场比赛时间: 日期{nextMatchDayTime} 时间{nextMatchAMOrPM} {nextMatchTime}时 赛区{nextMatchLeague}[/green]")
         except Exception:
             self.log.error("Q_Q 获取下一场比赛时间失败")
             print(f"[red]Q_Q 获取下一场比赛时间失败[/red]")
-            print_exc()
