cfa1dfe1bf6796c1972978eed84541d8db56d123
Yudaotor <yudaotor@qq.com>
2023-04-03 18:53:05 +0800

    feat: smartly play youtube
    当youtube为暂停时按播放按钮

--- EsportsHelper/Youtube.py
+++ EsportsHelper/Youtube.py
@@ -12,16 +12,32 @@ class Youtube:
         self.driver = driver
         self.log = log
 
-    def setYoutubeQuality(self) -> bool:
+    def playYoutubeStream(self):
         try:
             wait = WebDriverWait(self.driver, 15)
             wait.until(ec.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe[id=video-player-youtube]")))
             playButton = wait.until(ec.presence_of_element_located((By.XPATH, "/html/body/div/div/div[27]/div[2]/div[1]/button")))
-            try:
-                playButton.click()
-            except Exception:
-                self.driver.execute_script("arguments[0].click();", playButton)
-            sleep(1)
+            # 当检测到视频暂停时，点击播放按钮
+            streamStatus = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "button.ytp-play-button.ytp-button > svg > use")))
+            if streamStatus.get_attribute("href") == "#ytp-id-44":
+                try:
+                    playButton.click()
+                except Exception:
+                    self.driver.execute_script("arguments[0].click();", playButton)
+            # 离开播放器iframe
+            self.driver.switch_to.default_content()
+            return True
+        except TimeoutException:
+            self.log.error(format_exc())
+            return False
+        except Exception:
+            self.log.error(format_exc())
+            return False
+
+    def setYoutubeQuality(self) -> bool:
+        try:
+            wait = WebDriverWait(self.driver, 15)
+            wait.until(ec.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe[id=video-player-youtube]")))
             settingsButton = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "button[data-tooltip-target-id=ytp-settings-button]")))
             self.driver.execute_script("arguments[0].click();", settingsButton)
             sleep(1)
@@ -32,10 +48,10 @@ class Youtube:
             self.driver.execute_script("arguments[0].click();", option)
             self.driver.switch_to.default_content()
             return True
-        except TimeoutException as e:
+        except TimeoutException:
             self.log.error(format_exc())
             return False
-        except Exception as e:
+        except Exception:
             print_exc()
             self.log.error(format_exc())
             return False
