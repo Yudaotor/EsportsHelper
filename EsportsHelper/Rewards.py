39ed0b269b1742fa468b2e0704c869439b10fb28
Yudaotor <yudaotor@qq.com>
2023-04-03 22:06:23 +0800

    perf: change drop notif
    改变方式，可能导致功能失效，没测试（不过无所谓）

--- EsportsHelper/Rewards.py
+++ EsportsHelper/Rewards.py
@@ -6,13 +6,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import TimeoutException
 from rich import print
+from EsportsHelper.Utils import debugScreen
 
 
 class Rewards:
-    def __init__(self, log, driver, config) -> None:
+    def __init__(self, log, driver, config, youtube) -> None:
         self.log = log
         self.driver = driver
         self.config = config
+        self.youtube = youtube
 
     def isRewardMarkExist(self):
         wait = WebDriverWait(self.driver, 25)
@@ -23,7 +25,7 @@ class Rewards:
             return False
         return True
 
-    def checkRewards(self, url, retryTimes=6) -> bool:
+    def checkRewards(self, stream, url, retryTimes=6) -> bool:
         splitUrl = url.split('/')
         if splitUrl[-2] != "live":
             match = splitUrl[-2]
@@ -40,6 +42,8 @@ class Rewards:
                     print(
                         f"[yellow]×××××[/yellow] {match} 观看异常 [yellow]××××× 重试中...")
                     self.driver.refresh()
+                    if stream == "youtube":
+                        self.youtube.playYoutubeStream()
                 else:
                     self.log.error(f"××××× {match} 观看异常 ××××× url={url}")
                     print(f"[red]×××××[/red] {match} 观看异常 [red]××××× url={url}")
@@ -47,30 +51,33 @@ class Rewards:
 
     def checkNewDrops(self):
         try:
-            self.driver.implicitly_wait(15)
-
+            self.driver.implicitly_wait(5)
             isDrop = False
             poweredByImg = []
             productImg = []
-            unlockedDate = []
             eventTitle = []
             dropItem = []
             dropItemImg = []
-            drops = self.driver.find_elements(by=By.CSS_SELECTOR, value="div[class=drops-fulfilled]")
-            for drop in drops:
-                isDrop = True
-                drop.click()
-                time.sleep(2)
-                poweredByImg.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=sponsor-image] > img[class=img]"))
-                productImg.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=product-image] > img[class=img]"))
-                unlockedDate.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=product-image] > div[class=unlocked-date]"))
-                eventTitle.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=product-image] > div[class=title]"))
-                dropItem.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=reward] > div[class=wrapper] > div[class=title]"))
-                dropItemImg.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=reward] > div[class=image] > img[class=img]"))
-                self.driver.refresh()
+            unlockedDate = []
+            drops = self.driver.find_elements(by=By.CSS_SELECTOR, value="div.InformNotifications > div > div.product-image > img")
+            if len(drops) > 0:
+                for i in range(len(drops)):
+                    isDrop = True
+                    debugScreen(self.driver, "before click")
+                    drops[i].click()
+                    debugScreen(self.driver, "after click")
+                    time.sleep(2)
+                    poweredByImg.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=presented-by] > img[class=img]").get_attribute("src"))
+                    productImg.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=product-image] > img[class=img]").get_attribute("src"))
+                    eventTitle.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div.RewardsDropsCard > div > div.title.short").text)
+                    dropItem.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=reward] > div[class=wrapper] > div[class=title]").text)
+                    unlockedDate.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div.RewardsDropsCard > div > div.unlocked-date").text)
+                    dropItemImg.append(self.driver.find_element(by=By.CSS_SELECTOR, value="div[class=reward] > div[class=image] > img[class=img]").get_attribute("src"))
+                    closeButton = self.driver.find_element(by=By.CSS_SELECTOR, value="div.RewardsDropsOverlay > div.close")
+                    closeButton.click()
             if isDrop:
                 self.driver.implicitly_wait(15)
-                return isDrop, poweredByImg, productImg, unlockedDate, eventTitle, dropItem, dropItemImg
+                return isDrop, poweredByImg, productImg, eventTitle, unlockedDate, dropItem, dropItemImg
             else:
                 self.driver.implicitly_wait(15)
                 return isDrop, [], [], [], [], [], []
@@ -79,49 +86,73 @@ class Rewards:
             self.log.error("〒.〒 检查掉落失败")
             traceback.print_exc()
             print("[red]〒.〒 检查掉落失败[/red]")
-            return False, [], [], [], [], [], []
+            return False, [], [], [], [], [], [], []
 
-    def notifyDrops(self, poweredByImg, productImg, unlockedDate, eventTitle, dropItem, dropItemImg):
+    def notifyDrops(self, poweredByImg, productImg, eventTitle, unlockedDate, dropItem, dropItemImg):
         try:
-            for i in range(len(poweredByImg)):
-                if "https://oapi.dingtalk.com" in self.config.connectorDropsUrl:
-                    data = {
-                        "msgtype": "link",
-                        "link": {
-                            "text": "Drop掉落提醒",
-                            "title": f"[{self.config.username}]通过事件{eventTitle[i]} 获得{dropItem[i]} {unlockedDate[i]}",
-                            "picUrl": f"{dropItemImg[i]}",
-                            "messageUrl": "https://lolesports.com/rewards"
-                        }
+            s = requests.session()
+            s.keep_alive = False  # 关闭多余连接
+            if "https://oapi.dingtalk.com" in self.config.connectorDropsUrl:
+                data = {
+                    "msgtype": "link",
+                    "link": {
+                        "text": "Drop掉落提醒",
+                        "title": f"[{self.config.username}]通过事件{eventTitle} 获得{dropItem} {unlockedDate}",
+                        "picUrl": f"{dropItemImg}",
+                        "messageUrl": "https://lolesports.com/rewards"
                     }
-                    requests.post(self.config.connectorDropsUrl, json=data)
-                    time.sleep(5)
-                elif "https://discord.com/api/webhooks" in self.config.connectorDropsUrl:
-                    embed = {
-                        "title": "掉落提醒",
-                        "description": f"[{self.config.username}]通过事件{eventTitle[i]} 获得{dropItem[i]} {unlockedDate[i]}",
-                        "image": {"url": f"{productImg[i]}"},
-                        "thumbnail": {"url": f"{dropItemImg}"},
-                        "color": 6676471,
-                    }
-                    params = {
-                        "username": "EsportsHelper",
-                        "embeds": [embed]
-                    }
-                    requests.post(self.config.connectorDropsUrl, headers={"Content-type": "application/json"}, json=params)
-                    time.sleep(5)
-                elif "https://fwalert.com" in self.config.connectorDropsUrl:
-                    params = {
-                        "text": f"[{self.config.username}]通过事件{eventTitle[i]} 获得{dropItem[i]} {unlockedDate[i]}",
-                    }
-                    requests.post(self.config.connectorDropsUrl, headers={"Content-type": "application/json"}, json=params)
-                    time.sleep(5)
-                else:
-                    params = {
-                        "text": f"[{self.config.username}]通过事件{eventTitle[i]} 获得{dropItem[i]} {unlockedDate[i]}",
-                    }
-                    requests.post(self.config.connectorDropsUrl, headers={"Content-type": "application/json"}, json=params)
-                    time.sleep(5)
+                }
+                s.post(self.config.connectorDropsUrl, json=data)
+                time.sleep(5)
+            elif "https://discord.com/api/webhooks" in self.config.connectorDropsUrl:
+                field1 = {
+                    "name": "Event",
+                    "value": f"{eventTitle}",
+                    "inline": True
+                }
+                field2 = {
+                    "name": "Reward",
+                    "value": f"{dropItem}",
+                    "inline": True
+                }
+                field4 = {
+                    "name": "Unlocked-Date",
+                    "value": f"{unlockedDate}",
+                    "inline": True
+                }
+                fieldNone = {
+                    "name": "",
+                    "value": "",
+                    "inline": True
+                }
+                embed = {
+                    "title": "掉落提醒",
+                    "description": f"{self.config.username}",
+                    "image": {"url": f"{productImg}"},
+                    "thumbnail": {"url": f"{dropItemImg}"},
+                    "fields": [field1, field2, fieldNone, field4, fieldNone, fieldNone],
+                    "color": 6676471,
+                    "timestamp": time.strftime("%Y-%m-%dT%H:%M:%S+08:00", time.localtime())
+                }
+                params = {
+                    "username": "EsportsHelper",
+                    "embeds": [embed]
+                }
+                s.post(self.config.connectorDropsUrl, headers={"Content-type": "application/json"}, json=params)
+                time.sleep(5)
+            elif "https://fwalert.com" in self.config.connectorDropsUrl:
+                params = {
+                    "text": f"[{self.config.username}]通过事件{eventTitle} 获得{dropItem} {unlockedDate}",
+                }
+                s.post(self.config.connectorDropsUrl, headers={"Content-type": "application/json"}, json=params)
+                time.sleep(5)
+            else:
+                params = {
+                    "text": f"[{self.config.username}]通过事件{eventTitle} 获得{dropItem} {unlockedDate}",
+                }
+                s.post(self.config.connectorDropsUrl, headers={"Content-type": "application/json"}, json=params)
+                time.sleep(5)
+            self.log.info(">_< 掉落提醒成功")
         except Exception:
             self.log.error("〒.〒 掉落提醒失败")
             traceback.print_exc()
