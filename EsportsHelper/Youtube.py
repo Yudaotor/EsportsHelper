1057f918e9168e3ac62d4c289aa97d6144a0b3ee
TriangleWS <dskamay@gmail.com>
2023-04-16 21:46:31 +0400

    Formatting

--- EsportsHelper/Youtube.py
+++ EsportsHelper/Youtube.py
@@ -15,15 +15,19 @@ class Youtube:
     def playYoutubeStream(self):
         try:
             wait = WebDriverWait(self.driver, 15)
-            wait.until(ec.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe[id=video-player-youtube]")))
-            playButton = wait.until(ec.presence_of_element_located((By.XPATH, "/html/body/div/div/div[27]/div[2]/div[1]/button")))
+            wait.until(ec.frame_to_be_available_and_switch_to_it(
+                (By.CSS_SELECTOR, "iframe[id=video-player-youtube]")))
+            playButton = wait.until(ec.presence_of_element_located(
+                (By.XPATH, "/html/body/div/div/div[27]/div[2]/div[1]/button")))
             # 当检测到视频暂停时，点击播放按钮
-            streamStatus = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "button.ytp-play-button.ytp-button > svg > use")))
+            streamStatus = wait.until(ec.presence_of_element_located(
+                (By.CSS_SELECTOR, "button.ytp-play-button.ytp-button > svg > use")))
             if streamStatus.get_attribute("href") == "#ytp-id-44":
                 try:
                     playButton.click()
                 except Exception:
-                    self.driver.execute_script("arguments[0].click();", playButton)
+                    self.driver.execute_script(
+                        "arguments[0].click();", playButton)
             # 离开播放器iframe
             self.driver.switch_to.default_content()
             return True
@@ -37,14 +41,18 @@ class Youtube:
     def setYoutubeQuality(self) -> bool:
         try:
             wait = WebDriverWait(self.driver, 15)
-            wait.until(ec.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe[id=video-player-youtube]")))
-            settingsButton = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, "button[data-tooltip-target-id=ytp-settings-button]")))
+            wait.until(ec.frame_to_be_available_and_switch_to_it(
+                (By.CSS_SELECTOR, "iframe[id=video-player-youtube]")))
+            settingsButton = wait.until(ec.presence_of_element_located(
+                (By.CSS_SELECTOR, "button[data-tooltip-target-id=ytp-settings-button]")))
             self.driver.execute_script("arguments[0].click();", settingsButton)
             sleep(1)
-            qualityButton = wait.until(ec.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[25]/div/div/div[2]/div[3]")))
+            qualityButton = wait.until(ec.presence_of_element_located(
+                (By.XPATH, "/html/body/div[1]/div/div[25]/div/div/div[2]/div[3]")))
             self.driver.execute_script("arguments[0].click();", qualityButton)
             sleep(1)
-            option = wait.until(ec.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[25]/div/div[2]/div[6]/div")))
+            option = wait.until(ec.presence_of_element_located(
+                (By.XPATH, "/html/body/div[1]/div/div[25]/div/div[2]/div[6]/div")))
             self.driver.execute_script("arguments[0].click();", option)
             self.driver.switch_to.default_content()
             return True
