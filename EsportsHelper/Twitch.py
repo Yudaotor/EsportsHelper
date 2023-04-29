407355aa4950e269e142c2028f2e2b5231240241
Yudaotor <yudaotor@qq.com>
2023-04-29 21:36:39 +0800

    perf: add wait to self

--- EsportsHelper/Twitch.py
+++ EsportsHelper/Twitch.py
@@ -1,5 +1,5 @@
 from time import sleep
-from traceback import format_exc, print_exc
+from traceback import format_exc
 
 from rich import print
 from selenium.common import TimeoutException
@@ -12,16 +12,16 @@ class Twitch:
     def __init__(self, driver, log) -> None:
         self.driver = driver
         self.log = log
+        self.wait = WebDriverWait(self.driver, 20)
 
     def setTwitchQuality(self) -> bool:
         try:
-            wait = WebDriverWait(self.driver, 15)
-            wait.until(ec.frame_to_be_available_and_switch_to_it(
+            self.wait.until(ec.frame_to_be_available_and_switch_to_it(
                 (By.CSS_SELECTOR, "iframe[title=Twitch]")))
             sleep(3)
             self.driver.implicitly_wait(15)
             isMute = self.driver.find_elements(By.CSS_SELECTOR, "button[data-a-target=player-mute-unmute-button] > div > div > div > svg > g")
-            muteButton = wait.until(ec.presence_of_element_located(
+            muteButton = self.wait.until(ec.presence_of_element_located(
                 (By.CSS_SELECTOR, "button[data-a-target=player-mute-unmute-button]")))
             if len(isMute) <= 0:
                 try:
@@ -33,15 +33,15 @@ class Twitch:
                     print("Twitch: UnMute")
                     self.log.info("Twitch: UnMute")
             sleep(1)
-            settingsButton = wait.until(ec.presence_of_element_located(
+            settingsButton = self.wait.until(ec.presence_of_element_located(
                 (By.CSS_SELECTOR, "button[data-a-target=player-settings-button]")))
             self.driver.execute_script("arguments[0].click();", settingsButton)
             sleep(1)
-            qualityButton = wait.until(ec.presence_of_element_located(
+            qualityButton = self.wait.until(ec.presence_of_element_located(
                 (By.CSS_SELECTOR, "button[data-a-target=player-settings-menu-item-quality]")))
             self.driver.execute_script("arguments[0].click();", qualityButton)
             sleep(1)
-            options = wait.until(ec.presence_of_all_elements_located(
+            options = self.wait.until(ec.presence_of_all_elements_located(
                 (By.CSS_SELECTOR, "input[data-a-target=tw-radio]")))
             self.driver.execute_script("arguments[0].click();", options[-1])
             sleep(1)
