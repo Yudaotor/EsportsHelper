2e2f8ec5dc02009454e09e8b65f18577c8d095b3
Yudaotor <yudaotor@qq.com>
2024-02-19 00:23:25 +0800

    feat: turn on logging about performance and debug functions

--- EsportsHelper/Webdriver.py
+++ EsportsHelper/Webdriver.py
@@ -29,93 +29,104 @@ def getDriverVersion(chromeDriverManager):
     return version
 
 
+def addWebdriverOptions(options):
+    """
+    Adds options to a Chrome webdriver instance.
+
+    Args:
+    - options: An instance of ChromeOptions to which the options will be added.
+
+    Returns:
+    - An instance of ChromeOptions with the added options.
+
+    Raises:
+    - None
+    """
+    options.add_argument("--disable-extensions")
+    options.add_argument('--disable-audio-output')
+    options.add_argument('--autoplay-policy=no-user-gesture-required')
+    options.add_argument("--disable-gpu")
+    options.debugger_address = "127.0.0.1:9222"
+    options.set_capability("goog:loggingPrefs", {
+        'performance': 'ALL'
+    })
+    options.set_capability("goog:perfLoggingPrefs", {
+        'enableNetwork': True,
+        'enablePage': False,
+        'enableTimeline': False
+    })
+
+    prefs = {
+        "profile.password_manager_enabled": False,
+        "credentials_enable_service": False,
+    }
+    options.add_experimental_option('prefs', prefs)
+    if config.proxy:
+        options.add_argument(f"--proxy-server={config.proxy}")
+    if config.headless and not config.isDockerized:
+        options.add_argument("--headless=new")
+
+        windowsAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/118.0.2088.88"
+        macAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+        linuxAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+
+        if config.platForm == "windows":
+            userAgent = windowsAgent
+        elif config.platForm == "mac":
+            userAgent = macAgent
+        elif config.platForm == "linux":
+            userAgent = linuxAgent
+        else:
+            # Default to one agent, just in case
+            userAgent = windowsAgent
+
+        options.add_argument(f'user-agent={userAgent}')
+    return options
+
+
+def createWebdriver():
+    """
+    Creates a webdriver instance of uc.Chrome with specified options and configurations.
+
+    Returns:
+        A uc.Chrome instance.
+    """
+    customPath = ".\\driver"
+    chromeDriverManager = ChromeDriverManager(cache_manager=DriverCacheManager(customPath))
+    if config.isDockerized:
+        driverPath = "/undetected_chromedriver/chromedriver"
+    else:
+        if config.platForm == "linux":
+            if config.arm64:
+                username = os.getlogin()
+                driverPath = f"/home/{username}/.local/share/undetected_chromedriver/chromedriver"
+                if not os.path.exists(driverPath):
+                    log.error(_log("找不到 chromedriver"))
+                    return
+            else:
+                customPath = "driver"
+                chromeDriverManager = ChromeDriverManager(cache_manager=DriverCacheManager(customPath))
+                driverPath = chromeDriverManager.install()
+        elif config.platForm == "windows":
+            driverPath = chromeDriverManager.install()
+        else:
+            log.error(_("不支持的操作系统"))
+
+    options = addWebdriverOptions(uc.ChromeOptions())
+    print(_("正在准备中...", color="yellow"))
+    log.info(_log("正在准备中..."))
+    version = getDriverVersion(chromeDriverManager)
+
+    kwargs = {
+        "options": options,
+        "driver_executable_path": driverPath,
+        "version_main": version,
+        "browser_executable_path": config.chromePath if config.chromePath else None,
+        "user_data_dir": config.userDataDir if config.userDataDir else None,
+    }
+    return uc.Chrome(**{k: v for k, v in kwargs.items() if v})
+
+
 class Webdriver:
     def __init__(self) -> None:
-        self.config = config
-        self.log = log
-
-    def createWebdriver(self):
-        """
-        Creates a webdriver instance of uc.Chrome with specified options and configurations.
-
-        Returns:
-            A uc.Chrome instance.
-        """
-        customPath = ".\\driver"
-        chromeDriverManager = ChromeDriverManager(cache_manager=DriverCacheManager(customPath))
-        if self.config.isDockerized:
-            driverPath = "/undetected_chromedriver/chromedriver"
-        else:
-            if self.config.platForm == "linux":
-                if self.config.arm64:
-                    username = os.getlogin()
-                    driverPath = f"/home/{username}/.local/share/undetected_chromedriver/chromedriver"
-                    if not os.path.exists(driverPath):
-                        self.log.error(_log("找不到 chromedriver"))
-                        return
-                else:
-                    customPath = "driver"
-                    chromeDriverManager = ChromeDriverManager(cache_manager=DriverCacheManager(customPath))
-                    driverPath = chromeDriverManager.install()
-            elif self.config.platForm == "windows":
-                driverPath = chromeDriverManager.install()
-            else:
-                self.log.error(_("不支持的操作系统"))
-
-        options = self.addWebdriverOptions(uc.ChromeOptions())
-        print(_("正在准备中...", color="yellow"))
-        self.log.info(_log("正在准备中..."))
-        version = getDriverVersion(chromeDriverManager)
-
-        kwargs = {
-            "options": options,
-            "driver_executable_path": driverPath,
-            "version_main": version,
-            "browser_executable_path": self.config.chromePath if self.config.chromePath else None,
-            "user_data_dir": self.config.userDataDir if self.config.userDataDir else None,
-        }
-        return uc.Chrome(**{k: v for k, v in kwargs.items() if v})
-
-    def addWebdriverOptions(self, options):
-        """
-        Adds options to a Chrome webdriver instance.
-
-        Args:
-        - options: An instance of ChromeOptions to which the options will be added.
-
-        Returns:
-        - An instance of ChromeOptions with the added options.
-
-        Raises:
-        - None
-        """
-        options.add_argument("--disable-extensions")
-        options.add_argument('--disable-audio-output')
-        options.add_argument('--autoplay-policy=no-user-gesture-required')
-        options.add_argument("--disable-gpu")
-        prefs = {
-            "profile.password_manager_enabled": False,
-            "credentials_enable_service": False,
-        }
-        options.add_experimental_option('prefs', prefs)
-        if self.config.proxy:
-            options.add_argument(f"--proxy-server={self.config.proxy}")
-        if self.config.headless and not self.config.isDockerized:
-            options.add_argument("--headless=new")
-            
-            windows_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/118.0.2088.88"
-            mac_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
-            linux_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
-            
-            if self.config.platForm == "windows":
-                user_agent = windows_agent
-            elif self.config.platForm == "mac":
-                user_agent = mac_agent
-            elif self.config.platForm == "linux":
-                user_agent = linux_agent
-            else:
-                # Default to one agent, just in case
-                user_agent = windows_agent
-                
-            options.add_argument(f'user-agent={user_agent}')
-        return options
+        pass
