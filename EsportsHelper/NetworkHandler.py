a49c78b2ec4b899be59190494afcc3ef67585500
ReformedDoge <39759213+ReformedDoge@users.noreply.github.com>
2024-08-14 15:29:26 +0300

    Revert "Updated content-type check in the condition to handle both "application/json" and "text/plain" packet types."
    This reverts commit afff2cfbb91897aa88820623a4f1e228ba2411a9.

--- EsportsHelper/NetworkHandler.py
+++ EsportsHelper/NetworkHandler.py
@@ -35,7 +35,7 @@ def getRewardByLog(driver, isInit=False):
             if message.get('method') != 'Network.responseReceived':
                 continue
             packetType = message.get('params').get('response').get('mimeType')
-            if packetType != "application/json" and packetType != "text/plain":
+            if packetType != "application/json":
                 continue
             requestId = message.get('params').get('requestId')
             url = message.get('params').get('response').get('url')
